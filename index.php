f8b881f1c2a2bd63de1d02400b73bb90df1b6aed
Airton Jr <airtonbjunior@gmail.com>
2016-07-18 18:25:38 -0300

    MVC Practice from scratch
    MVC Practice from scratch - Use this project to learn and teach the
    basic flow in a MVC project.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	require 'vendor/autoload.php';
+	$controller = new App\Mvc\Controller();
+
+
+	$controller->index();
+
+?>
---
+++ src/App/Mvc/Controller.php
@@ -0,0 +1,13 @@
+<?php
+	namespace App\Mvc;
+
+	class Controller 
+	{
+
+		// Public methods of the controller are called "Actions"
+		public function index() {
+			echo 'Hello, World';
+		}	
+	}
+
+?>
